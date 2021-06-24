b19ed4c4dab525dcaa5272bf6e0940086aef8b0c
FlyingLobster69 <73574038+arthurjin01@users.noreply.github.com>
2021-06-24 15:49:00 -0700

    v1.4.6

--- commands/windows.js
+++ commands/windows.js
@@ -2,7 +2,7 @@ module.exports = {
     checkWindows: function(receivedMessage) {
     const {MessageAttachment} = require('discord.js')
     const chilledwindows = new MessageAttachment('win11_installer.exe') // creates windows installer message attachment
-    const win11 = new MessageAttachment('win11bk.jpg')
+    const win11 = new MessageAttachment('win11bk.png')
     let fullCommand = receivedMessage.content.substr(4) // Remove the leading pog
     let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after "pog" is the command
