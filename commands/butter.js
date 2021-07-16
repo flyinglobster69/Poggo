2fc3e30f26d639ad54c1751be3607c77732ad8be
arthurjin01 <arthurjin01@outlook.com>
2021-07-15 19:22:10 -0700

    v1.7.1 - Updated wish sim

--- commands/butter.js
+++ commands/butter.js
@@ -1,15 +1,17 @@
 module.exports = {
     checkButter: function(receivedMessage) {
 
-    const {MessageEmbed} = require('discord.js')
+    const {MessageEmbed, MessageAttachment} = require('discord.js')
 
     let fullCommand = receivedMessage.content.substr(4) // Remove the leading pog
     let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after "pog" is the command
+    const butter = new MessageAttachment('butter.mp4')
     found = false
 
         switch(primaryCommand.toLowerCase()) { // called when messages says 'pog butter'
             case 'butter' :
+                receivedMessage.channel.send(butter)
                 const embed = new MessageEmbed()
                 .setTitle('mmmm b u t t e r 🛬')
                 .setDescription('https://youtu.be/lowrM-780tg 🧈')
