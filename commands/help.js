32598a20f2d7b7e0d2b6902706b507a7ec38a109
FlyingLobster69 <arthurjin01@gmail.com>
2021-12-09 16:35:30 -0800

    v1.10.2 - Fixes a massive glitch that causes the bot to be unable to start

--- commands/help.js
+++ commands/help.js
@@ -109,7 +109,7 @@ Note: Superscripts are not enabled, therefore for powers, the exponent is denote
                 else {
                     const embed = new MessageEmbed()
                     .setTitle(`Prefix\: \`pog\``) 
-                    .setDescription(`Command list\: \nBot Commands: \`help\`, \`test\`, \`code\`, \`user\`, \`ping\`, \`version\`, \`readme\`, \`invite\`;
+                    .setDescription(`Command list\: \nBot Commands: \`help\`, \`test\`, \`code\`, \`profile\`, \`ping\`, \`version\`, \`readme\`, \`invite\`;
 Troll Commands: \`biden\`, \`trump\`, \`buff\`, \`pirate\`, \`start\`, \`butter\`, \`american\`, \`horny\`, \`isearthround\`, \`die\`;
 Bigger Commands\: \`exe [insert text here]\`, \`smol [insert genshin waifu here]\`, \`sm [insert character name here]\`, \`wish [10]\`;
 
