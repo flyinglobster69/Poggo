9518087d15332578bad12a57230c58d4a15316ed
arthurjin01 <arthurjin01@outlook.com>
2022-01-24 17:11:55 -0800

    v2.0 - Cleaned up unnecessary files

--- commands/remind.js
+++ commands/remind.js
@@ -1,3 +1,5 @@
+let reminderlist = []
+
 module.exports = {
     checkRemind: function(receivedMessage) {
     const {MessageEmbed} = require('discord.js')
@@ -5,47 +7,73 @@ module.exports = {
     let splitCommand = fullCommand.split(' ') // Split the message up in to pieces for each space
     let primaryCommand = splitCommand[0] // The first word directly after 'pog' is the command
     let arguments = splitCommand.slice(1)
-    var first = splitCommand.slice(1)
-    var second = splitCommand.slice(2)
-    var content = splitCommand.slice(3).join(' ')
-    const secondlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let first = splitCommand.slice(1)
+    let second = splitCommand.slice(2)
+    let content = splitCommand.slice(3).join(' ')
+    // const secondlist = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    let minute = parseInt(second)
+    let hour = parseInt(first)
+    let totalwait = (minute * 60000) + (hour * 3600000)
+    let index = reminderlist.indexOf(content)
+
     found = false
 
     function remindUser() {
+        reminderlist.splice(index, 1)
+        console.log(index)
+        console.log(content)
+        console.log(reminderlist.splice(index, content))
         return receivedMessage.channel.send(`<@!${receivedMessage.author.id}> I am here to remind you to ${content} 🤔`)
     }
+    // function nums() {
+    //     return Array.from(String(second))
+    // }
 
         switch(primaryCommand.toLowerCase()) { // called when messages says 'pog root'
             case 'remind' :
 
-                if (secondlist.includes(second)) {
-                    var minute = parseInt(second)
-                    var hour = parseInt(first)
-                    var totalwait = (minute * 60000) + (hour * 3600000)
-
+                if (arguments.toString().toLowerCase() == 'list') {
                     const embed = new MessageEmbed()
-                    .setTitle('Reminder Set')
-                    .setDescription(`<@!${receivedMessage.author.id}> I will remind you to ${content} in ${hour} hours and ${minute} minutes.`)
+                    .setTitle('Active Reminders')
+                    .setDescription(reminderlist.join(`
+                    `))
                     .setColor('#00ADEF')
                     .setTimestamp()
                     receivedMessage.reply({ embeds: [embed]})
+
+                    // receivedMessage.reply('**Active Reminders: ** `' + reminderlist.join('` `') + '`')
+                    
                 }
-                // else if (arguments.toLowerCase() == 'list') {
+                // else if (nums().includes(secondlist)) {
+                //     // const embed = new MessageEmbed()
+                //     // .setTitle('Active Reminders')
+                //     // .setDescription(reminderlist[0].toString())
+                //     // .setColor('#00ADEF')
+                //     // .setTimestamp()
+                //     // receivedMessage.reply({ embeds: [embed]})
+
+                //     minute
+                //     hour
+                //     totalwait
+                //     reminderlist.push(content)
+                //     index = reminderlist.indexOf(content)
+
                 //     const embed = new MessageEmbed()
-                //     .setTitle('Active Reminders')
-                //     .setDescription()
+                //     .setTitle('Reminder Set')
+                //     .setDescription(`<@!${receivedMessage.author.id}> I will remind you to ${content} in ${hour} hours and ${minute} minutes.`)
                 //     .setColor('#00ADEF')
                 //     .setTimestamp()
                 //     receivedMessage.reply({ embeds: [embed]})
                 // }
                 else {
-                    var content = splitCommand.slice(2).join(' ')
-                    var minute = parseInt(first)
-                    var totalwait = minute * 60000
+                    // var totalwait = minute * 60000
+                    totalwait
+                    reminderlist.push(content)
+                    index += 1
 
                     const embed = new MessageEmbed()
                     .setTitle('Reminder Set')
-                    .setDescription(`<@!${receivedMessage.author.id}> I will remind you to ${content} in ${minute} minutes.`)
+                    .setDescription(`<@!${receivedMessage.author.id}> I will remind you to ${content} in ${hour} hours and ${minute} minutes.`)
                     .setColor('#00ADEF')
                     .setTimestamp()
                     receivedMessage.reply({ embeds: [embed]})
